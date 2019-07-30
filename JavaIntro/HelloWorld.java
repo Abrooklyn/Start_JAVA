5e6688fa228ff3c6226a6ea05d59f4633291e2bc
Angelique Vannespenne <angeliquevannespenne@hotmail.fr>
2019-07-30 12:25:22 +0200

    First Lesson

---
+++ JavaIntro/HelloWorld.java
@@ -0,0 +1,12 @@
+class HelloWorld {
+
+    public static void main(String[] args) {
+        // afficher Hello World! dans le terminal
+        System.out.println("Hello World!");
+    }
+    if (age > 17){
+        System.out.println("Majeur");
+    } else {
+        System.out.println("Mineur");
+    }
+}
