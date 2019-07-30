c56ddfd446b953962a13bcdb68447f2e9837054d
Angelique Vannespenne <angeliquevannespenne@hotmail.fr>
2019-07-30 15:11:31 +0200

    Indiana Example / Variable

---
+++ Indiana/Indiana.java
@@ -0,0 +1,16 @@
+class Indiana {
+
+    public static void main(String[] args) {
+
+        String movieTitle = "Indiana Jones and the Last Crusade";
+        boolean seenMovie= true;
+        int yearMovie = 1989;
+        float noteImdb = 8.2f;
+
+        System.out.println("Title : " + movieTitle);
+        System.out.println("Seen movie : " + seenMovie);
+        System.out.println("Year : " + yearMovie);
+        System.out.println("IMDB note : " + noteImdb);
+    }
+
+}
